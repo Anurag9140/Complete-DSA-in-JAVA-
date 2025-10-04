d2c37cc8cf4504686ef83293b4f26597ccba1b70
ANURAG VERMA <152633495+Anurag9140@users.noreply.github.com>
2025-10-04 15:06:54 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,13 @@
+
+public class variables {
+    public static void main(String[] args) {
+        int x=5;
+        System.out.println(x);
+        System.out.println(x +"anurag verma");
+
+        x=x+5;
+        System.out.println(x);
+        System.out.println(x*5);
+        
+    }
+}
