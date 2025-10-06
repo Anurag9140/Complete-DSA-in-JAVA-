62daf933377a9ab5f23cba10dc1fd0967eb8222e
ANURAG VERMA <152633495+Anurag9140@users.noreply.github.com>
2025-10-06 14:06:49 +0530

    Add files via upload

---
+++ arithmetic_operators.java
@@ -0,0 +1,22 @@
+public class arithmetic_operators {
+    public static void main(String[] args) {
+        // double x = 10, y = 20;
+        // System.out.println(x+y);
+        // System.out.println(x-y);
+        // System.out.println(x*y);
+        // System.out.println(x/y);
+
+
+        // Area of a circle
+        // double r=5;
+        // double area=3.14*r*r;
+        // System.out.println(area);
+
+
+        // Volume of Sphere
+        double radius=12;
+        double volume=(4.0/3)*3.1415*radius*radius*radius;
+        System.out.println(volume);
+
+    }
+}
