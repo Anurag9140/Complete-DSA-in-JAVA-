8379b7f36dd7395c4fdcfd4454b88dec72709e40
ANURAG VERMA <152633495+Anurag9140@users.noreply.github.com>
2025-10-06 14:09:45 +0530

    Add files via upload

---
+++ Absolute_Value.java
@@ -0,0 +1,21 @@
+package if_else;
+import java.util.Scanner;
+public class Absolute_Value {
+     static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+         System.out.println("Enter number");
+        int n=sc.nextInt();
+        if(n>=0){
+            System.out.println(n);
+        }
+        else{
+            System.out.println(n*-1);
+        }
+
+    }
+
+    public static class isInteger {
+    }
+}
+
+
