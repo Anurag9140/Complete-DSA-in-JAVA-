65fdb346fccc31bbed9bd126507aaae815b6ae4f
ANURAG VERMA <152633495+Anurag9140@users.noreply.github.com>
2025-10-11 19:12:23 +0530

    Add files via upload

---
+++ Basic_Loops.java
@@ -0,0 +1,24 @@
+
+
+public class Basic_Loops {
+    public static void main(String[] args) {
+        for(int i=19;i<=190;i+=19){
+            System.out.println(i);
+        }
+
+        for(int i=1;i<=10;i++){
+            System.out.println("ANURAG VERMA");
+        }
+
+        // Print all Even Numbers btw 1 to 100
+        for(int i=2;i<=100;i+=2){
+            System.out.print(i+" ");
+        }
+
+        // Decreasing Loops
+        for(int i=10;i>=1;i--){
+            System.out.print(i+" ");
+        }
+    }
+
+}
---
+++ Geometr_Progr.java
@@ -0,0 +1,9 @@
+public class Geometr_Progr {
+    public static void main(String[] args) {
+        int a=1,r=2;
+        for(int i=1;i<=5;i++){
+            System.out.println(a);
+            a*=r;
+        }
+    }
+}
